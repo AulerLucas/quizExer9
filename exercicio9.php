9a2e15cd0d5ae8b386a84545b11bd12fa7f6af27
AulerLucas <lucasaulerbonfante@yahoo.com.br>
2022-11-21 18:10:47 -0300

    :zap:

---
+++ exercicio9.php
@@ -0,0 +1,13 @@
+<?php
+$s = $_POST['string'];
+$num = strlen($s);
+echo "Palavra: " . $s."<br>";
+
+for ($l = 1; $l <= $num; $l++) {
+    for ($c = 1; $c <= $l; $c++) {
+        echo $s. " ";
+    }
+    echo "<br>";
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title> exercicio 9</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <form action="exercicio9.php" method="POST">
+
+            <div id="til">
+                <h1> Exercício 5</h1>
+            </div>
+
+            <p id="p">
+                Insira uma palavra: <input type="text" name="string" />
+            </p>
+
+            <div id="bt">
+                <input type="submit" value="Resultado" name='res' id="res">
+            </div>
+    </div>
+    </form>
+</body>
+
+</html>
